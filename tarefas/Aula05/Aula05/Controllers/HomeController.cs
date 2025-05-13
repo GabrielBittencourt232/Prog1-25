f49a28b3f5e01b1dbd09b3e864c4511b92bdd5f0
Gabriel Bittencourt <gabrielbittencourt1003@hotmail.com>
2025-05-12 22:04:50 -0300

    aula 08

---
+++ tarefas/Aula05/Aula05/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Aula05.Models;
+using Model;
+
+namespace Aula05.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        Customer c1 = new Customer();
+        c1.Name = "Frodo";
+        c1.ObjectCount++;
+        Customer.InstanceCount++;
+
+        var c2 = new Customer();
+        c2.Name = "Galadriel";
+        c2.ObjectCount++;
+        Customer.InstanceCount++;
+
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ tarefas/Aula05/Model/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace Model
+{
+   public class Order
+    {
+        public int Id { get; set; }
+        public Customer? Customer { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string? ShippingAddress { get; set; }
+        public List<OrderItem>? OrderItems { get; set; }
+
+    }
+}
