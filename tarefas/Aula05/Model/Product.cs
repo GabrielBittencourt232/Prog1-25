f239723e100ded694e2ac0fdba07aaf7d2de7eef
Gabriel Bittencourt <gabrielbittencourt1003@hotmail.com>
2025-05-19 19:48:51 -0300

    atividade

--- tarefas/Aula05/Model/Product.cs
+++ tarefas/Aula05/Model/Product.cs
@@ -7,9 +7,13 @@
         public string? Description { get; set; }
         public double CurrentPrice { get; set; }
 
-        public bool Validade()
+        public bool Validate()
         {
-            return true;
+            bool isValid = true;
+
+            isValid = !string.IsNullOrEmpty(this.Name) && (this.Id > 0) && (this.CurrentPrice > 0);
+
+            return isValid;
         }
 
         public Product Retrieve()
