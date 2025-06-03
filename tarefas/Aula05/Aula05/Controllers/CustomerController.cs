96556e235e0b6217056784cfd40f2452aafb992e
Gabriel Bittencourt <gabrielbittencourt1003@hotmail.com>
2025-06-02 21:21:01 -0300

    Novas coisas

--- tarefas/Aula05/Aula05/Controllers/CustomerController.cs
+++ tarefas/Aula05/Aula05/Controllers/CustomerController.cs
@@ -6,11 +6,14 @@ namespace Aula05.Controllers
 {
     public class CustomerController : Controller
     {
+        private readonly IWebHostEnvironment enrironment;
+
         private CustomerRepository _customerRepository;
 
-        public CustomerController()
+        public CustomerController(IWebHostEnvironment enrironment)
         {
             _customerRepository = new CustomerRepository();
+            this.enrironment = enrironment;
         }
 
         [HttpGet]
@@ -34,6 +37,50 @@ namespace Aula05.Controllers
         [HttpGet]
         public IActionResult Create()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult ExportDelimitatedFile()
+        {
+            string fileContent = string.Empty;
+            foreach (Customer c in CustomerData.Customers)
+            {
+                fileContent +=
+                    @$"{c.Id};{c.Name};
+                    {c.HomeAddress.Id};
+                    {c.HomeAddress.City};
+                    {c.HomeAddress.State};
+                    {c.HomeAddress.Country};
+                    {c.HomeAddress.Street1};
+                    {c.HomeAddress.Street2};
+                    {c.HomeAddress.PostalCode};
+                    {c.HomeAddress.AddressType}
+                    \n
+                    ";
+            }
+
+            var path = Path.Combine(
+                    enrironment.WebRootPath,
+                    "TextFiles"
+            );
+
+            if (!System.IO.Directory.Exists(path))
+                System.IO.Directory.CreateDirectory(path);
+
+            var filepath = Path.Combine(
+                    path,
+                    "Delimitado.txt"
+            );
+
+            if (!System.IO.File.Exists(filepath))
+            {
+                using (StreamWriter sw = System.IO.File.CreateText(filepath))
+                {
+                    sw.Write(fileContent);
+                }
+            }
+
             return View();
         }
     }
